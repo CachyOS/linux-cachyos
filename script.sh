a8dee68b688dc0490566306b3b9598b7678c4d08
P. Jung <ptr1337@cachyos.org>
2021-10-25 09:54:47 +0000

    TT Scheduler

--- script.sh
+++ script.sh
@@ -17,26 +17,36 @@ done
 
 mv */*.tar.zst* /home/ptr1337/packages/
 
-
-find . -name "PKGBUILD" | xargs -I {} sed -i "s/_use_llvm_lto=/_use_llvm_lto==y/" {}
+find . -name "config" | xargs -I {} sed -i 's/GENERIC_CPU=y/GENERIC_CPU3=y/' {}
 
 files=$(find . -name "PKGBUILD")
 
 for f in $files
-
 do
-
   d=$(dirname $f)
-
   cd $d
 
-  docker run --name dockerbuild -e EXPORT_PKG=1 -v $PWD:/pkg pttrr/docker-makepkg
-
+  docker run --name dockerbuild -e EXPORT_PKG=1 -v $PWD:/pkg pttrr/docker-makepkg-v3
   docker rm dockerbuild
-
   cd ..
-
 done
 
 mv */*.tar.zst* /home/ptr1337/packages/
 
+
+
+find . -name "PKGBUILD" | xargs -I {} sed -i "s/_use_llvm_lto=/_use_llvm_lto==y/" {}
+
+
+files=$(find . -name "PKGBUILD")
+
+for f in $files
+do
+  d=$(dirname $f)
+  cd $d
+  docker run --name dockerbuild -e EXPORT_PKG=1 -v $PWD:/pkg pttrr/docker-makepkg-v3
+  docker rm dockerbuild
+  cd ..
+done
+
+mv */*.tar.zst* /home/ptr1337/packages/
