66affaa23a69df0a5485e94e91e44134675b6f1c
Peter Jung <admin@ptr1337.dev>
2022-04-27 16:01:30 +0200

    5.17.5-1

--- script.sh
+++ script.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 find . -name "PKGBUILD" | xargs -I {} sed -i "s/_use_auto_optimization=y/_use_auto_optimization=/" {}
+find . -name "config" | xargs -I {} sed -i 's/GENERIC_CPU=y/GENERIC_CPU3=y/' {}
 
 
 files=$(find . -name "PKGBUILD")
@@ -10,14 +11,14 @@ do
   d=$(dirname $f)
   cd $d
 
-  docker run --name kernelbuild -e EXPORT_PKG=1 -v $PWD:/pkg -v /home/ptr1337/ccache-kernel-kernel:/home/notroot/ccache pttrr/docker-makepkg-kernel
+  docker run --name kernelbuild -e EXPORT_PKG=1 -v $PWD:/pkg -v /home/ptr1337/ccache-kernel:/home/notroot/ccache pttrr/docker-makepkg-kernel
   docker rm kernelbuild
   cd ..
 done
 
 mv */*.tar.zst* /home/ptr1337/packages/
 
-find . -name "config" | xargs -I {} sed -i 's/GENERIC_CPU=y/GENERIC_CPU3=y/' {}
+find . -name "config" | xargs -I {} sed -i 's/GENERIC_CPU3=y/GENERIC_CPU=y/' {}
 
 files=$(find . -name "PKGBUILD")
 
@@ -36,7 +37,7 @@ mv */*.tar.zst* /home/ptr1337/packages/
 
 
 find . -name "PKGBUILD" | xargs -I {} sed -i "s/_use_llvm_lto=/_use_llvm_lto=thin/" {}
-
+find . -name "config" | xargs -I {} sed -i '$_thin_lto_cachedir=$_thin_lto_cachedir=y/' {}
 
 files=$(find . -name "PKGBUILD")
 
@@ -51,7 +52,7 @@ done
 
 mv */*.tar.zst* /home/ptr1337/packages/
 
-find . -name "config" | xargs -I {} sed -i 's/GENERIC_CPU3=y/GENERIC_CPU=y/' {}
+find . -name "config" | xargs -I {} sed -i 's/GENERIC_CPU=y/GENERIC_CPU3=y/' {}
 
 files=$(find . -name "PKGBUILD")
 
