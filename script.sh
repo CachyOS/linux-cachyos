39371f453d186822f98cd9580e581f274a311fb9
Peter Jung <admin@ptr1337.dev>
2022-05-09 20:57:23 +0200

    5.17.6-2-fix-lto-zfs

--- script.sh
+++ script.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 find . -name "PKGBUILD" | xargs -I {} sed -i "s/_use_auto_optimization=y/_use_auto_optimization=/" {}
-find . -name "config" | xargs -I {} sed -i 's/GENERIC_CPU=y/GENERIC_CPU3=y/' {}
-
+find . -name "PKGBUILD" | xargs -I {} sed -i "s/_use_llvm_lto=/_use_llvm_lto=thin/" {}
+find . -name "PKGBUILD" | xargs -I {} sed -i "s/_thinlto_cachedir=/_thin_lto_cachedir=y/" {}
 
 files=$(find . -name "PKGBUILD")
 
@@ -17,7 +17,8 @@ done
 
 mv */*.tar.zst* /home/ptr1337/packages/
 
-find . -name "config" | xargs -I {} sed -i 's/GENERIC_CPU3=y/GENERIC_CPU=y/' {}
+find . -name "config" | xargs -I {} sed -i 's/GENERIC_CPU=y/GENERIC_CPU3=y/' {}
+
 
 files=$(find . -name "PKGBUILD")
 
@@ -32,10 +33,9 @@ done
 
 mv */*.tar.zst* /home/ptr1337/packages/
 
-
-
-find . -name "PKGBUILD" | xargs -I {} sed -i "s/_use_llvm_lto=/_use_llvm_lto=thin/" {}
-find . -name "config" | xargs -I {} sed -i '$_thin_lto_cachedir=$_thin_lto_cachedir=y/' {}
+find . -name "config" | xargs -I {} sed -i 's/GENERIC_CPU3=y/GENERIC_CPU=y/' {}
+find . -name "PKGBUILD" | xargs -I {} sed -i "s/_use_llvm_lto=thin/_use_llvm_lto=/" {}
+find . -name "PKGBUILD" | xargs -I {} sed -i "s/_thinlto_cachedir=y/_thin_lto_cachedir=/" {}
 
 files=$(find . -name "PKGBUILD")
 
@@ -43,7 +43,7 @@ for f in $files
 do
     d=$(dirname $f)
     cd $d
-    docker run --name kernelbuild -e EXPORT_PKG=1 -v $PWD:/pkg -v /home/ptr1337/ccache-kernel:/home/notroot/ccache pttrr/docker-makepkg-kernel-v3
+    docker run --name kernelbuild -e EXPORT_PKG=1 -v $PWD:/pkg -v /home/ptr1337/ccache-kernel:/home/notroot/ccache pttrr/docker-makepkg-kernel
     docker rm kernelbuild
     cd ..
 done
