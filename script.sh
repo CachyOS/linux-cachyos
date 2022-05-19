23426a790f4002983ff346f95a45f954af13c5ae
Peter Jung <admin@ptr1337.dev>
2022-05-19 20:34:42 +0200

    fix script

--- script.sh
+++ script.sh
@@ -39,14 +39,14 @@ for f in $files
 do
     d=$(dirname $f)
     cd $d
-    docker run --name kernelbuild -e EXPORT_PKG=1 -v $PWD:/pkg -v /home/ptr1337/ccache-kernel:/home/notroot/.buildcache pttrr/docker-makepkg
+    docker run --name kernelbuild -e EXPORT_PKG=1 -v $PWD:/pkg -v /home/ptr1337/ccache-kernel:/home/notroot/.buildcache pttrr/docker-makepkg-v3
     docker rm kernelbuild
     cd ..
 done
 
 mv */*.tar.zst* /home/ptr1337/packages/
 
-find . -name "config" | xargs -I {} sed -i 's/GENERIC_CPU=y/GENERIC_CPU3=y/' {}
+find . -name "config" | xargs -I {} sed -i 's/GENERIC_CPU3=y/GENERIC_CPU=y/' {}
 
 files=$(find . -name "PKGBUILD")
 
