9c01fb0fb278f1adf99cfa5785e4da3c375e7cda
Peter Jung <admin@ptr1337.dev>
2022-05-19 20:59:42 +0200

    enhance script to update repo's automatically

--- script.sh
+++ script.sh
@@ -11,8 +11,6 @@ do
     cd ..
 done
 
-mv */*.tar.zst* /home/ptr1337/packages/
-
 find . -name "config" | xargs -I {} sed -i 's/GENERIC_CPU=y/GENERIC_CPU3=y/' {}
 
 
@@ -26,8 +24,6 @@ do
     docker rm kernelbuild
     cd ..
 done
-
-mv */*.tar.zst* /home/ptr1337/packages/
 
 find . -name "PKGBUILD" | xargs -I {} sed -i "s/_use_auto_optimization=y/_use_auto_optimization=/" {}
 find . -name "PKGBUILD" | xargs -I {} sed -i "s/_use_llvm_lto=/_use_llvm_lto=thin/" {}
@@ -44,8 +40,6 @@ do
     cd ..
 done
 
-mv */*.tar.zst* /home/ptr1337/packages/
-
 find . -name "config" | xargs -I {} sed -i 's/GENERIC_CPU3=y/GENERIC_CPU=y/' {}
 
 files=$(find . -name "PKGBUILD")
@@ -59,4 +53,8 @@ do
     cd ..
 done
 
-mv */*.tar.zst* /home/ptr1337/packages/
+mv */*-x86_64.pkg.tar.zst* /home/ptr1337/.docker/build/nginx/www/repo/x86_64/cachyos/
+repoctl update -P cachyos
+
+mv *-x86_64_v3.pkg.tar.zst* /home/ptr1337/.docker/build/nginx/www/repo/x86_64_v3/cachyos-v3/
+repoctl update -P cachyos-v3
