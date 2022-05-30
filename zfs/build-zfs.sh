39b03c6a15780fd0ccafd05a4cbcffbe209dab76
Peter Jung <admin@ptr1337.dev>
2022-05-30 13:10:36 +0200

    5.18.1-1

--- zfs/build-zfs.sh
+++ zfs/build-zfs.sh
@@ -46,6 +46,18 @@ docker rm kernelbuild-zfs
 docker run --name kernelbuild-zfs -e EXPORT_PKG=1 -v $PWD:/pkg pttrr/docker-makepkg-v3
 docker rm kernelbuild-zfs
 
+find . -name "PKGBUILD" | xargs -I {} sed -i "s/_pkgcore=cacule-rdb/_pkgcore=tt/" {}
+docker run --name kernelbuild-zfs -e EXPORT_PKG=1 -v $PWD:/pkg pttrr/docker-makepkg
+docker rm kernelbuild-zfs
+docker run --name kernelbuild-zfs -e EXPORT_PKG=1 -v $PWD:/pkg pttrr/docker-makepkg-v3
+docker rm kernelbuild-zfs
+
+find . -name "PKGBUILD" | xargs -I {} sed -i "s/_pkgcore=tt/_pkgcore=cachyos-tt/" {}
+docker run --name kernelbuild-zfs -e EXPORT_PKG=1 -v $PWD:/pkg pttrr/docker-makepkg
+docker rm kernelbuild-zfs
+docker run --name kernelbuild-zfs -e EXPORT_PKG=1 -v $PWD:/pkg pttrr/docker-makepkg-v3
+docker rm kernelbuild-zfs
+
 mv *-x86_64.pkg.tar.zst* /home/ptr1337/.docker/build/nginx/www/repo/x86_64/cachyos/
 repoctl update -P cachyos
 
