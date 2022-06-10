abd9803cafa190dfd21e784f5b818ca48d4c586a
Peter Jung <admin@ptr1337.dev>
2022-06-10 19:06:35 +0200

    fix autodetection for all cpu's

--- linux-cachyos-hardened/auto-cpu-optimization.sh
+++ linux-cachyos-hardened/auto-cpu-optimization.sh
@@ -1,53 +1,36 @@
 #!/bin/bash
-CPU=$(gcc -Q -march=native --help=target|grep mtune=|awk '{print $2}'|head -1)
+CPU=$(gcc -Q -march=native --help=target|grep march=|awk '{print $2}'|head -1)
 MARCH=$(echo $CPU|tr '[:lower:]' '[:upper:]'&&echo)
-if [[ ${MARCH} == "ZNVER" ]]
-then
-	MARCH="ZEN"
-
-elif [[ ${MARCH} == "ZNVER2" ]]
-then
-	MARCH="ZEN2"
-
-elif [[ ${MARCH} == "ZNVER3" ]]
-then
-	MARCH="ZEN3"
-elif [[ ${MARCH} == "BDVER1" ]]
-then
-	MARCH="BULLDOZER"
-elif [[ ${MARCH} == "BDVER2" ]]
-	MARCH="PILEDRIVER"
-then
-elif [[ ${MARCH} == "BDVER3" ]]
-	MARCH="STEAMROLLER"
-then
-elif [[ ${MARCH} == "BDVER4" ]]
-	MARCH="EXCAVATOR"
-then
-elif [[ ${MARCH} == "BTVER1" ]]
-then
-	MARCH="BOBCAT"
-elif [[ ${MARCH} == "BTVER2" ]]
-	MARCH="JAGUAR"
-then
-elif [[ ${MARCH} == "AMDFAM10" ]]
-	MARCH="MK10"
-then
-elif [[ ${MARCH} == "K8-SSE3" ]]
-	MARCH="K8SSE3"
-then
-elif [[ ${MARCH} == "BONNELL" ]]
-	MARCH="ATOM"
-then
-elif [[ ${MARCH} == "GOLDMONT-PLUS" ]]
-	MARCH="GOLDMONTPLUS"
-then
-elif [[ ${MARCH} == "SKYLAKE-AVX512" ]]
-	MARCH="SKYLAKE2"
-then
-elif [[ ${MARCH} == "ICELAKE-CLIENT" ]]
-	MARCH="ICELAKE"
-then
+if [[ ${MARCH} == "ZNVER" ]]; then
+    MARCH="ZEN"
+elif [[ ${MARCH} == "ZNVER2" ]]; then
+    MARCH="ZEN2"
+elif [[ ${MARCH} == "ZNVER3" ]]; then
+    MARCH="ZEN3"
+elif [[ ${MARCH} == "BDVER1" ]]; then
+    MARCH="BULLDOZER"
+elif [[ ${MARCH} == "BDVER2" ]]; then
+    MARCH="PILEDRIVER"
+elif [[ ${MARCH} == "BDVER3" ]]; then
+    MARCH="STEAMROLLER"
+elif [[ ${MARCH} == "BDVER4" ]]; then
+    MARCH="EXCAVATOR"
+elif [[ ${MARCH} == "BTVER1" ]]; then
+    MARCH="BOBCAT"
+elif [[ ${MARCH} == "BTVER2" ]]; then
+    MARCH="JAGUAR"
+elif [[ ${MARCH} == "AMDFAM10" ]]; then
+    MARCH="MK10"
+elif [[ ${MARCH} == "K8-SSE3" ]]; then
+    MARCH="K8SSE3"
+elif [[ ${MARCH} == "BONNELL" ]]; then
+    MARCH="ATOM"
+elif [[ ${MARCH} == "GOLDMONT-PLUS" ]]; then
+    MARCH="GOLDMONTPLUS"
+elif [[ ${MARCH} == "SKYLAKE-AVX512" ]]; then
+    MARCH="SKYLAKE2"
+elif [[ ${MARCH} == "ICELAKE-CLIENT" ]]; then
+    MARCH="ICELAKE"
 fi
 MARCH2=M${MARCH}
 echo
