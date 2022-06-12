be86f8f04e910d311715d91a915f915983aa16ec
Peter Jung <admin@ptr1337.dev>
2022-06-12 04:25:05 +0200

    add bcachefs as option

--- script.sh
+++ script.sh
@@ -8,7 +8,7 @@ for f in $files
 do
     d=$(dirname $f)
     cd $d
-    docker run --name kernelbuild -e EXPORT_PKG=1 -v $PWD:/pkg -v /home/ptr1337/ccache-kernel:/home/notroot/.buildcache pttrr/docker-makepkg
+    docker run --name kernelbuild -e EXPORT_PKG=1 LLVM_BOLT=1 -v $PWD:/pkg -v /home/ptr1337/ccache-kernel:/home/notroot/.buildcache pttrr/docker-makepkg
     docker rm kernelbuild
     cd ..
 done
@@ -22,7 +22,7 @@ for f in $files
 do
     d=$(dirname $f)
     cd $d
-    docker run --name kernelbuild -e EXPORT_PKG=1 -v $PWD:/pkg -v /home/ptr1337/ccache-kernel:/home/notroot/.buildcache pttrr/docker-makepkg-v3
+    docker run --name kernelbuild -e EXPORT_PKG=1 LLVM_BOLT=1 -v $PWD:/pkg -v /home/ptr1337/ccache-kernel:/home/notroot/.buildcache pttrr/docker-makepkg-v3
     docker rm kernelbuild
     cd ..
 done
@@ -36,7 +36,7 @@ for f in $files
 do
     d=$(dirname $f)
     cd $d
-    docker run --name kernelbuild -e EXPORT_PKG=1 -v $PWD:/pkg -v /home/ptr1337/ccache-kernel:/home/notroot/.buildcache pttrr/docker-makepkg-v3
+    docker run --name kernelbuild -e EXPORT_PKG=1 LLVM_BOLT=1 -v $PWD:/pkg -v /home/ptr1337/ccache-kernel:/home/notroot/.buildcache pttrr/docker-makepkg-v3
     docker rm kernelbuild
     cd ..
 done
@@ -49,7 +49,7 @@ for f in $files
 do
     d=$(dirname $f)
     cd $d
-    docker run --name kernelbuild -e EXPORT_PKG=1 -v $PWD:/pkg -v /home/ptr1337/ccache-kernel:/home/notroot/.buildcache pttrr/docker-makepkg
+    docker run --name kernelbuild -e EXPORT_PKG=1 LLVM_BOLT=1 -v $PWD:/pkg -v /home/ptr1337/ccache-kernel:/home/notroot/.buildcache pttrr/docker-makepkg
     docker rm kernelbuild
     cd ..
 done
