c552a61ee5624d4ed5727cf2650abd9c6b9c3c22
Peter Jung <admin@ptr1337.dev>
2022-12-06 18:28:01 +0100

    Drop LTO support for Generic(v1) from the repo
    Signed-off-by: Peter Jung <admin@ptr1337.dev>

--- script.sh
+++ script.sh
@@ -2,11 +2,10 @@
 find . -name "PKGBUILD" | xargs -I {} sed -i "s/_use_auto_optimization-y/_use_auto_optimization-/" {}
 ## Enable ZFS
 find . -name "PKGBUILD" | xargs -I {} sed -i "s/_build_zfs-/_build_zfs-y/" {}
-#find . -name "PKGBUILD" | xargs -I {} sed -i "s/_bcachefs=/_bcachefs=y/" {} # breaks probably /proc/meminfo
 ## Enable Generic v3 
 find . -name "config" | xargs -I {} sed -i 's/GENERIC_CPU=y/GENERIC_CPU3=y/' {}
-## Dont compile linux-cacule linux-cacule-rdb into the repo
-rm -rf linux-cacule linux-cacule-rdb
+## Dont compile linux-cacule linux-cacule-rdb linux-cachyos-cacule into the repo
+rm -rf linux-cacule linux-cacule-rdb linux-cachyos-cacule
 
 ## GCC v3 Kernel
 
@@ -21,9 +20,12 @@ do
     cd ..
 done
 
-find . -name "PKGBUILD" | xargs -I {} sed -i "s/_use_llvm_lto-/_use_llvm_lto-thin/" {}
+echo "move kernels to the repo"
+mv */*-x86_64_v3.pkg.tar.zst* /home/ptr1337/.docker/build/nginx/www/repo/x86_64_v3/cachyos-v3/
+repoctl update -P cachyos-v3
 
 ## LLVM v3 Kernel
+find . -name "PKGBUILD" | xargs -I {} sed -i "s/_use_llvm_lto-/_use_llvm_lto-thin/" {}
 
 files=$(find . -name "PKGBUILD")
 
@@ -41,22 +43,8 @@ mv */*-x86_64_v3.pkg.tar.zst* /home/ptr1337/.docker/build/nginx/www/repo/x86_64_
 repoctl update -P cachyos-v3
 
 
-## LLVM Generic Kernel
+## GCC Generic Kernel
 find . -name "config" | xargs -I {} sed -i 's/GENERIC_CPU3=y/GENERIC_CPU=y/' {}
-
-files=$(find . -name "PKGBUILD")
-
-for f in $files
-do
-    d=$(dirname $f)
-    cd $d
-    time docker run --name kernelbuild -e EXPORT_PKG=1 -e SYNC_DATABASE=1 -e CHECKSUMS=1 -v /home/ptr1337/docker-makepkg/new/docker-makepkg/llvm:/home/notroot/llvm -v $PWD:/pkg -v /home/ptr1337/kernelbuild/ccache-kernel-llvm:/home/notroot/.buildcache pttrr/docker-makepkg
-    docker rm kernelbuild
-    cd ..
-done
-
-## Generic Kernel
-
 find . -name "PKGBUILD" | xargs -I {} sed -i "s/_use_llvm_lto-thin/_use_llvm_lto-/" {}
 
 files=$(find . -name "PKGBUILD")
