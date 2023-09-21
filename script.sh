195ba21f78e5bcfd4ca93230bcf7a5082a969ca0
ptr1337 <admin@ptr1337.dev>
2023-09-21 09:46:02 +0200

    repo-build-script: Compile LTO Kernels also for v4

--- script.sh
+++ script.sh
@@ -24,7 +24,7 @@ echo "move kernels to the repo"
 mv */*-x86_64_v3.pkg.tar.zst* /home/ptr1337/.docker/build/nginx/www/repo/x86_64_v3/cachyos-v3/
 repoctl update -P cachyos-v3
 
-## LLVM v3 Kernel
+## LLVM ThinLTO v3 Kernel
 find . -name "PKGBUILD" | xargs -I {} sed -i "s/_use_llvm_lto-none/_use_llvm_lto-thin/" {}
 
 files=$(find . -name "PKGBUILD")
@@ -81,3 +81,20 @@ echo "move kernels to the repo"
 mv */*-x86_64_v4.pkg.tar.zst* /home/ptr1337/.docker/build/nginx/www/repo/x86_64_v4/cachyos-v4/
 repoctl update -P cachyos-v4
 
+## LLVM ThinLTO v4 Kernel
+find . -name "PKGBUILD" | xargs -I {} sed -i "s/_use_llvm_lto-none/_use_llvm_lto-thin/" {}
+
+files=$(find . -name "PKGBUILD")
+
+for f in $files
+do
+    d=$(dirname $f)
+    cd $d
+    time docker run --name kernelbuild -e EXPORT_PKG=1 -e SYNC_DATABASE=1 -e CHECKSUMS=1 -v /home/ptr1337/docker-makepkg/llvm:/home/notroot/llvm -v $PWD:/pkg pttrr/docker-makepkg-v4
+    docker rm kernelbuild
+    cd ..
+done
+
+echo "move kernels to the repo"
+mv */*-x86_64_v4.pkg.tar.zst* /home/ptr1337/.docker/build/nginx/www/repo/x86_64_v4/cachyos-v4/
+repoctl update -P cachyos-v4
