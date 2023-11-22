b159febb871cbfa46af80dd0d0cbb7ba32866c03
Peter Jung <admin@ptr1337.dev>
2023-11-22 18:19:18 +0100

    script: Use different build script for v1/v3-v4
    Signed-off-by: Peter Jung <admin@ptr1337.dev>

--- script.sh
+++ script.sh
@@ -2,50 +2,8 @@
 find . -name "PKGBUILD" | xargs -I {} sed -i "s/_use_auto_optimization-y/_use_auto_optimization-/" {}
 ## Enable ZFS
 find . -name "PKGBUILD" | xargs -I {} sed -i "s/_build_zfs-/_build_zfs-y/" {}
-## Enable Generic v3 
-find . -name "config" | xargs -I {} sed -i 's/GENERIC_CPU=y/GENERIC_CPU3=y/' {}
 ## Enable NVIDIA module
 find . -name "PKGBUILD" | xargs -I {} sed -i "s/_build_nvidia-/_build_nvidia-y/" {}
-
-## GCC v3 Kernel
-
-files=$(find . -name "PKGBUILD")
-
-for f in $files
-do
-    d=$(dirname $f)
-    cd $d
-    time docker run --name kernelbuild -e EXPORT_PKG=1 -e SYNC_DATABASE=1 -e CHECKSUMS=1 -v /home/ptr1337/docker-makepkg/llvm:/home/notroot/llvm -v $PWD:/pkg pttrr/docker-makepkg-v3
-    docker rm kernelbuild
-    cd ..
-done
-
-echo "move kernels to the repo"
-mv */*-x86_64_v3.pkg.tar.zst* /home/ptr1337/.docker/build/nginx/www/repo/x86_64_v3/cachyos-v3/
-repoctl update -P cachyos-v3
-
-## LLVM ThinLTO v3 Kernel
-find . -name "PKGBUILD" | xargs -I {} sed -i "s/_use_llvm_lto-none/_use_llvm_lto-thin/" {}
-
-files=$(find . -name "PKGBUILD")
-
-for f in $files
-do
-    d=$(dirname $f)
-    cd $d
-    time docker run --name kernelbuild -e EXPORT_PKG=1 -e SYNC_DATABASE=1 -e CHECKSUMS=1 -v /home/ptr1337/docker-makepkg/llvm:/home/notroot/llvm -v $PWD:/pkg pttrr/docker-makepkg-v3
-    docker rm kernelbuild
-    cd ..
-done
-
-echo "move kernels to the repo"
-mv */*-x86_64_v3.pkg.tar.zst* /home/ptr1337/.docker/build/nginx/www/repo/x86_64_v3/cachyos-v3/
-repoctl update -P cachyos-v3
-
-
-## GCC Generic Kernel
-find . -name "config" | xargs -I {} sed -i 's/GENERIC_CPU3=y/GENERIC_CPU=y/' {}
-find . -name "PKGBUILD" | xargs -I {} sed -i "s/_use_llvm_lto-thin/_use_llvm_lto-none/" {}
 
 files=$(find . -name "PKGBUILD")
 
@@ -61,40 +19,3 @@ done
 echo "move kernels to the repo"
 mv */*-x86_64.pkg.tar.zst* /home/ptr1337/.docker/build/nginx/www/repo/x86_64/cachyos/
 repoctl update -P cachyos
-
-
-## GCC v4 Kernel
-find . -name "config" | xargs -I {} sed -i 's/GENERIC_CPU=y/GENERIC_CPU4=y/' {}
-
-files=$(find . -name "PKGBUILD")
-
-for f in $files
-do
-    d=$(dirname $f)
-    cd $d
-    time docker run --name kernelbuild -e EXPORT_PKG=1 -e SYNC_DATABASE=1 -e CHECKSUMS=1 -v /home/ptr1337/docker-makepkg/llvm:/home/notroot/llvm -v $PWD:/pkg pttrr/docker-makepkg-v4
-    docker rm kernelbuild
-    cd ..
-done
-
-echo "move kernels to the repo"
-mv */*-x86_64_v4.pkg.tar.zst* /home/ptr1337/.docker/build/nginx/www/repo/x86_64_v4/cachyos-v4/
-repoctl update -P cachyos-v4
-
-## LLVM ThinLTO v4 Kernel
-find . -name "PKGBUILD" | xargs -I {} sed -i "s/_use_llvm_lto-none/_use_llvm_lto-thin/" {}
-
-files=$(find . -name "PKGBUILD")
-
-for f in $files
-do
-    d=$(dirname $f)
-    cd $d
-    time docker run --name kernelbuild -e EXPORT_PKG=1 -e SYNC_DATABASE=1 -e CHECKSUMS=1 -v /home/ptr1337/docker-makepkg/llvm:/home/notroot/llvm -v $PWD:/pkg pttrr/docker-makepkg-v4
-    docker rm kernelbuild
-    cd ..
-done
-
-echo "move kernels to the repo"
-mv */*-x86_64_v4.pkg.tar.zst* /home/ptr1337/.docker/build/nginx/www/repo/x86_64_v4/cachyos-v4/
-repoctl update -P cachyos-v4
