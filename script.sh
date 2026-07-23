d7262e8078467c64cd897ada9344cd6caf72ebec
Eric Naim <dnaim@cachyos.org>
2026-07-23 12:59:21 +0700

    build: Use new generic PKGBUILD
    Refactor the build scripts to make use of the newly written PKGBUILD.
    This removes 6 PKGBUILDs that are just there for different build
    configurations. LTS, RC and hardened PKGBUILDs are still kept because
    they differ by version and not configuration.
    
    Co-authored-by: Vasiliy Stelmachenok <ventureo@cachyos.org>
    Signed-off-by: Vasiliy Stelmachenok <ventureo@cachyos.org>
    Signed-off-by: Eric Naim <dnaim@cachyos.org>

--- script.sh
+++ script.sh
@@ -1,24 +1,33 @@
 #!/usr/bin/env bash
-## Enable Generic
-find . -name "PKGBUILD" | xargs -I {} sed -i "s/_processor_opt:=/_processor_opt:=GENERIC/" {}
-find . -name "PKGBUILD" | xargs -I {} sed -i "s/_use_auto_optimization:=yes/_use_auto_optimization:=no/" {}
-## Enable ZFS
-find . -name "PKGBUILD" | xargs -I {} sed -i "s/_build_zfs:=no/_build_zfs:=yes/" {}
-## Enable Open NVIDIA module
-find . -name "PKGBUILD" | xargs -I {} sed -i "s/_build_nvidia_open:=no/_build_nvidia_open:=yes/" {}
-## Enable r8125 module
-find . -name "PKGBUILD" | xargs -I {} sed -i "s/_build_r8125:=no/_build_r8125:=yes/" {}
+set -euo pipefail
 
-files=$(find . -name "PKGBUILD")
+build_pkg() {
+    local dir="$1"
+    shift 1
+    time docker run --rm --name kernelbuild \
+        -e EXPORT_PKG=1 -e SYNC_DATABASE=1 -e CHECKSUMS=1 \
+        -e _processor_opt=GENERIC \
+        -e _build_zfs=yes \
+        -e _build_nvidia_open=yes \
+        -e _build_r8125=yes \
+        "$@" \
+        -v "$PWD/$dir:/pkg" \
+        pttrr/docker-makepkg
+}
 
-for f in $files
-do
-    d=$(dirname $f)
-    cd $d
-    time docker run --name kernelbuild -e EXPORT_PKG=1 -e SYNC_DATABASE=1 -e CHECKSUMS=1 -v $PWD:/pkg pttrr/docker-makepkg
-    docker rm kernelbuild
-    cd ..
-done
+# Build PKGBUILDs that still exist as separate packages.
+build_pkg linux-cachyos-lts -e _use_lto_suffix=yes -e _use_gcc_suffix=no
+build_pkg linux-cachyos-rc -e _use_lto_suffix=no -e _use_gcc_suffix=yes
+build_pkg linux-cachyos-hardened -e _use_lto_suffix=yes -e _use_gcc_suffix=no
+
+# Build linux-cachyos variants that replaced removed flavor PKGBUILDs.
+build_pkg linux-cachyos -e "_flavor=cachyos" -e "_use_llvm_lto=none" -e _package_suffix=gcc
+build_pkg linux-cachyos -e "_flavor=cachyos-eevdf" -e _package_suffix=lto
+build_pkg linux-cachyos -e "_flavor=cachyos-bore" -e _package_suffix=lto
+build_pkg linux-cachyos -e "_flavor=cachyos-bmq" -e _package_suffix=lto
+build_pkg linux-cachyos -e "_flavor=cachyos-rt-bore" -e _package_suffix=lto
+build_pkg linux-cachyos -e "_flavor=cachyos-server" -e _package_suffix=lto
+build_pkg linux-cachyos -e "_flavor=cachyos-deckify" -e _package_suffix=lto
 
 echo "move kernels to the repo"
 mv */*-x86_64.pkg.tar.zst* /home/ptr1337/.docker/build/nginx/www/repo/x86_64/cachyos/
